d0d11ce0dab618b5b0b26d265430a718075f0436
fltzr <josh.salinas.j@outlook.com>
2023-10-29 13:34:39 -0400

    perf(build): :zap: optimize vite config build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import browserslist from 'browserslist';
 import { browserslistToTargets } from 'lightningcss';
@@ -9,27 +9,12 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsConfigPaths()],
+	plugins: [react(), tsConfigPaths(), splitVendorChunkPlugin()],
 
-	build: {
-		outDir: 'dist',
-		minify: 'terser',
-		cssMinify: 'lightningcss',
-		sourcemap: true,
-		terserOptions: {
-			compress: {
-				keep_infinity: true,
-				drop_console: true,
-			},
-		},
-
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					react: ['react', 'react-dom', 'react-router-dom'],
-					vendor: ['lodash', '@cloudscape-design'],
-				},
-			},
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('>= 0.25%')),
 		},
 	},
 
@@ -43,16 +28,37 @@ export default defineConfig({
 		open: true,
 	},
 
+	build: {
+		outDir: 'dist',
+		minify: 'terser',
+		cssMinify: 'lightningcss',
+		cssCodeSplit: true,
+		terserOptions: {
+			mangle: true,
+			sourceMap: true,
+			compress: {
+				keep_infinity: true,
+				drop_console: true,
+			},
+		},
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					if (id.includes('node_modules'))
+						return id
+							.toString()
+							.split('node_modules/')[1]
+							.split('/')[0]
+							.toString();
+				},
+			},
+			treeshake: true,
+		},
+	},
+
 	test: {
 		globals: true,
 		environment: 'happy-dom',
 		setupFiles: ['src/test/setup.ts'],
-	},
-
-	css: {
-		transformer: 'lightningcss',
-		lightningcss: {
-			targets: browserslistToTargets(browserslist('>= 0.25%')),
-		},
 	},
 });
