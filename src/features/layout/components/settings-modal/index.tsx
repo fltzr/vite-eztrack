b1dbf16a504f8f41511482795553a5c49334b8db
fltzr <josh.salinas.j@outlook.com>
2023-10-30 00:05:14 -0400

    add content for settings-modal

--- src/features/layout/components/settings-modal/index.tsx
+++ src/features/layout/components/settings-modal/index.tsx
@@ -1,4 +1,9 @@
 import Modal from '@cloudscape-design/components/modal';
+import Header from '@cloudscape-design/components/header';
+import Grid from '@cloudscape-design/components/grid';
+import RadioGroup from '@cloudscape-design/components/radio-group';
+import FormField from '@cloudscape-design/components/form-field';
+import Box from '@cloudscape-design/components/box';
 
 type SettingsModalProps = {
 	visible: boolean;
@@ -6,8 +11,39 @@ type SettingsModalProps = {
 };
 export const SettingsModal = ({ visible, onDismiss }: SettingsModalProps) => {
 	return (
-		<Modal visible={visible} onDismiss={onDismiss}>
-			<div>Settings</div>
+		<Modal
+			size="small"
+			visible={visible}
+			onDismiss={onDismiss}
+			header={<Header variant="h2" children={'User Settings'} />}
+		>
+			<Grid gridDefinition={[{ colspan: 6 }, { colspan: 6 }]}>
+				<Box margin={{ top: 'xs', bottom: 'l' }}>
+					<FormField label="Theme">
+						<RadioGroup
+							name="theme-radio-group"
+							value={null}
+							items={[
+								{ label: 'Light', value: 'light' },
+								{ label: 'Dark', value: 'dark' },
+							]}
+						/>
+					</FormField>
+				</Box>
+
+				<Box margin={{ top: 'xs', bottom: 'l' }}>
+					<FormField label="Density">
+						<RadioGroup
+							name="density-radio-group"
+							value={null}
+							items={[
+								{ label: 'Comfortable', value: 'comfortable' },
+								{ label: 'Compact', value: 'compact' },
+							]}
+						/>
+					</FormField>
+				</Box>
+			</Grid>
 		</Modal>
 	);
 };
