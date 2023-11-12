f39c45be25024ecfdfa4a22d293823036a62a1dc
fltzr <josh.salinas.j@outlook.com>
2023-11-12 13:48:53 -0500

    implement custom theming

---
+++ src-theme/theme-builder.js
@@ -0,0 +1,32 @@
+import { dirname, join } from 'path';
+import { buildThemedComponents } from '@cloudscape-design/components-themeable/theming';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const theme = {
+    tokens: {
+        borderRadiusAlert: '0px',
+        borderRadiusBadge: '0px',
+        borderRadiusButton: '0.5rem',
+        borderRadiusCalendarDayFocusRing: '0px',
+        borderRadiusContainer: '0px',
+        borderRadiusControlCircularFocusRing: '0px',
+        borderRadiusControlDefaultFocusRing: '0px',
+        borderRadiusDropdown: '0px',
+        borderRadiusFlashbar: '0px',
+        borderRadiusInput: '0px',
+        borderRadiusItem: '0px',
+        borderRadiusPopover: '0px',
+        borderRadiusTabsFocusRing: '0px',
+        borderRadiusTiles: '0px',
+        borderRadiusToken: '0px',
+        borderRadiusTutorialPanelItem: '0px',
+    },
+};
+
+buildThemedComponents({
+    theme,
+    outputDir: __dirname,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -6,59 +6,71 @@ import react from '@vitejs/plugin-react-swc';
 import browserslist from 'browserslist';
 import { browserslistToTargets } from 'lightningcss';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsConfigPaths(), splitVendorChunkPlugin()],
+    plugins: [react(), tsConfigPaths(), splitVendorChunkPlugin()],
 
-	css: {
-		transformer: 'lightningcss',
-		lightningcss: {
-			targets: browserslistToTargets(browserslist('>= 0.25%')),
-		},
-	},
+    resolve: {
+        alias: {
+            '@cloudscape-design/components': resolve(__dirname, './src-theme/components'),
+            '@cloudscape-design/design-tokens': resolve(
+                __dirname,
+                './src-theme/design-tokens',
+            ),
+        },
+    },
 
-	server: {
-		port: 3000,
-		open: true,
-	},
+    css: {
+        transformer: 'lightningcss',
+        lightningcss: {
+            targets: browserslistToTargets(browserslist('>= 0.25%')),
+        },
+    },
 
-	preview: {
-		port: 3000,
-		open: true,
-	},
+    server: {
+        strictPort: true,
+        port: 3000,
+        open: false,
+    },
 
-	build: {
-		outDir: 'dist',
-		minify: 'terser',
-		cssMinify: 'lightningcss',
-		cssCodeSplit: true,
-		terserOptions: {
-			mangle: true,
-			sourceMap: true,
-			compress: {
-				keep_infinity: true,
-				drop_console: true,
-			},
-		},
-		rollupOptions: {
-			output: {
-				manualChunks: (id) => {
-					if (id.includes('node_modules'))
-						return id
-							.toString()
-							.split('node_modules/')[1]
-							.split('/')[0]
-							.toString();
-				},
-			},
-			treeshake: true,
-		},
-	},
+    preview: {
+        port: 3000,
+        open: true,
+    },
 
-	test: {
-		globals: true,
-		environment: 'happy-dom',
-		setupFiles: ['src/test/setup.ts'],
-	},
+    build: {
+        outDir: 'dist',
+        minify: 'terser',
+        cssMinify: 'lightningcss',
+        cssCodeSplit: true,
+        terserOptions: {
+            mangle: true,
+            sourceMap: true,
+            compress: {
+                keep_infinity: true,
+                drop_console: true,
+            },
+        },
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules'))
+                        return id
+                            .toString()
+                            .split('node_modules/')[1]
+                            .split('/')[0]
+                            .toString();
+                },
+            },
+            treeshake: true,
+        },
+    },
+
+    test: {
+        globals: true,
+        environment: 'happy-dom',
+        setupFiles: ['src/test/setup.ts'],
+    },
 });
