5c38c1fcaa5c1c303ec6d13fdf5d7940722969aa
fltzr <josh.salinas.j@outlook.com>
2023-12-09 17:06:33 -0500

    Add preferences component

---
+++ src/common/components/preferences/index.tsx
@@ -0,0 +1,64 @@
+import {
+	CollectionPreferences,
+	type CollectionPreferencesProps,
+} from '@cloudscape-design/components';
+import {
+	createContentDisplayOptions,
+	createPageSizeOptions,
+	type TableColumnDefinition,
+} from '@/common/utils/table-utils';
+
+type PreferencesProps = {
+	resource: string;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	items: TableColumnDefinition<any>[];
+	preferences: CollectionPreferencesProps.Preferences;
+	setPreferences: CollectionPreferencesProps['onConfirm'];
+	disabled?: boolean;
+};
+export const Preferences = ({ ...props }: PreferencesProps) => {
+	const contentDisplayOptions = createContentDisplayOptions(props.items);
+	const pageSizeOptions = createPageSizeOptions(props.resource);
+
+	return (
+		<CollectionPreferences
+			disabled={props.disabled}
+			confirmLabel="Apply"
+			cancelLabel="Cancel"
+			preferences={props.preferences}
+			contentDisplayPreference={{ options: contentDisplayOptions }}
+			pageSizePreference={{ options: pageSizeOptions }}
+			wrapLinesPreference={{}}
+			stripedRowsPreference={{}}
+			contentDensityPreference={{}}
+			stickyColumnsPreference={{
+				firstColumns: {
+					title: 'Stick first column(s)',
+					description:
+						'Keep the first column(s) visible while scrolling horizontally',
+					options: [
+						{ label: 'None', value: 0 },
+						{ label: 'First column', value: 1 },
+						{ label: 'First two columns', value: 2 },
+					],
+				},
+				lastColumns: {
+					title: 'Stick last column',
+					description:
+						'Keep the last column visible while scrolling horizontally',
+					options: [
+						{ label: 'None', value: 0 },
+						{ label: 'Last column', value: 1 },
+					],
+				},
+			}}
+			onConfirm={(event) => {
+				if (props.setPreferences) {
+					props.setPreferences(event);
+				} else {
+					console.log('setPreferences is not defined');
+				}
+			}}
+		/>
+	);
+};
