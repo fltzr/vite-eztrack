9e3b00f3f184eece0954e1387563ccc90f47dd26
fltzr <josh.salinas.j@outlook.com>
2024-01-01 23:13:06 -0500

    Refactor routes in routes.tsx

--- src/common/router/routes.tsx
+++ src/common/router/routes.tsx
@@ -2,61 +2,70 @@ import type { RouteObject } from 'react-router-dom';
 import { constant } from 'lodash-es';
 import { AppContainer } from '@/app/app-container';
 import { ErrorComponent } from '@/common/layouts/error-component';
+import { Container } from '@/container';
 
 export const routes: RouteObject[] = [
 	{
-		path: '/',
-		element: <AppContainer />,
+		element: <Container />,
 		errorElement: <ErrorComponent />,
 		children: [
 			{
-				index: true,
-				lazy: () => import('@/features/home/pages'),
-				handle: {
-					title: constant('Home'),
-				},
-			},
-			{
-				path: 'account',
-				handle: {
-					title: constant('Account'),
-				},
+				path: '/',
+				element: <AppContainer />,
+				errorElement: <ErrorComponent />,
 				children: [
 					{
 						index: true,
-						lazy: () => import('@/features/auth/pages/profile'),
+						lazy: () => import('@/features/home/pages'),
+						handle: {
+							title: constant('Home'),
+						},
+					},
+					{
+						path: 'account',
+						handle: {
+							title: constant('Account'),
+						},
+						children: [
+							{
+								index: true,
+								lazy: () => import('@/features/auth/pages/profile'),
+							},
+						],
+					},
+					{
+						path: 'budget',
+						handle: {
+							title: constant('Budget'),
+						},
+						children: [
+							{
+								index: true,
+								lazy: () => import('@/features/budget/pages/budget'),
+							},
+						],
 					},
 				],
 			},
 			{
-				path: 'budget',
-				handle: {
-					title: constant('Budget'),
-				},
+				path: 'auth',
+				lazy: () => import('@/features/auth/components/shell'),
+				errorElement: <ErrorComponent />,
 				children: [
 					{
+						path: 'signin',
 						index: true,
-						lazy: () => import('@/features/budget/pages/budget'),
+						lazy: () => import('@/features/auth/pages/sign-in'),
+					},
+					{
+						path: 'signup',
+						lazy: () => import('@/features/auth/pages/sign-up'),
+					},
+					{
+						path: 'verify',
+						lazy: () => import('@/features/auth/pages/verify-email'),
 					},
 				],
-			},
-		],
-	},
-	{
-		path: 'auth',
-		lazy: () => import('@/features/auth/pages'),
-		children: [
-			{
-				path: 'signin',
-				lazy: () => import('@/features/auth/pages/sign-in'),
-			},
-			{
-				path: 'signup',
-				lazy: () => import('@/features/auth/pages/sign-up'),
-			},
-			{
-				path: 'verify',
-				lazy: () => import('@/features/auth/pages/verify-email'),
 			},
 		],
 	},
