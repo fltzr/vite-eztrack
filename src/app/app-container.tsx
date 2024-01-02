fbe00b84541a7fa98841fa2c03839514a636624b
fltzr <josh.salinas.j@outlook.com>
2024-01-01 23:11:16 -0500

    Add Loader component and useAuthVerification hook in AppContainer

--- src/app/app-container.tsx
+++ src/app/app-container.tsx
@@ -2,6 +2,8 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { I18nProvider } from '@cloudscape-design/components/i18n';
 import enMessages from '@cloudscape-design/components/i18n/messages/all.en.json';
 import { App } from '@/app';
+import { Loader } from '@/common/components/loader';
+import { useAuthVerification } from '@/features/auth/hooks/use-auth-verification';
 import { selectIsAuthenticated } from '@/features/auth/state/selectors';
 import { useAppSelector } from '@/common/hooks';
 import { Layout } from '@/features/layout';
@@ -9,7 +11,13 @@ import { HelpPanelProvider } from '../common/hooks/use-help-panel';
 
 export const AppContainer = () => {
 	const location = useLocation();
+	const { isVerifying, isInitializing } = useAuthVerification();
+
 	const isAuthenticated = useAppSelector(selectIsAuthenticated);
+
+	if (isVerifying || isInitializing) {
+		return <Loader />;
+	}
 
 	return (
 		<I18nProvider messages={[enMessages]}>
@@ -20,7 +28,14 @@ export const AppContainer = () => {
 					</HelpPanelProvider>
 				</Layout>
 			) : (
-				<Navigate replace to="/auth/signin" state={{ from: location }} />
+				<Navigate
+					replace
+					to="/auth/signin"
+					state={{
+						from: location.pathname,
+						attemptToAccessProtectedResource: true,
+					}}
+				/>
 			)}
 		</I18nProvider>
 	);
